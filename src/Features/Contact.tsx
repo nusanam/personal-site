56b0e8a8c29158df01501c2fd48d8415efc362ba
Ruth Anam <ruth_anam@premierinc.com>
2025-11-20 17:47:33 -0600

    update link for public read access s3 resume bucket

--- src/Features/Contact.tsx
+++ src/Features/Contact.tsx
@@ -18,7 +18,8 @@ const ContactSection = () => {
     >
       <div className='max-w-[1400px] mx-auto px-6 md:px-10 xl:px-20 text-center'>
         <p className='text-xl md:text-2xl font-light text-white/80 max-w-[800px] mx-auto mb-12'>
-          I'm deeply passionate about preventative health, system architecture, and pixel perfect UI. I'm open to remote-only opportunities.
+          I'm deeply passionate about preventative health, system architecture,
+          and pixel perfect UI. I'm open to remote-only opportunities.
         </p>
 
         <div className='flex flex-col sm:flex-row items-center justify-center gap-6'>
@@ -37,7 +38,7 @@ const ContactSection = () => {
             target='_blank'
             rel='noopener noreferrer'
             className='group px-8 py-4 bg-transparent border-2 border-white text-white rounded-full font-medium text-lg hover:bg-white hover:text-black transition-all duration-300 hover:scale-105'
-            href='https://ruth-anam-fullstack-resume.s3.us-east-2.amazonaws.com/Ruth%20Anam_resume_25.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIAYYLBQDJCITITBDUZ%2F20251119%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20251119T200905Z&X-Amz-Expires=300&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEBwaCXVzLWVhc3QtMiJHMEUCIBJFQnWInRPqZiGjcATcBge8X5Q%2B9EIqMufGA7mL5L%2BFAiEAgCZYIsH3PNJevqvB3uta5NCAoCanwlNBr8dQ2luGUZgq3wII5f%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARAEGgw2MDIwMzY3NzEzOTYiDNNdX9SEPW1dqXJMaSqzAoAWUTtS2aCXaXnO%2B5wfL7UqK5b4gFPhid%2FsGK8TJPTmv16IDnUotMFveDCpCmD%2BUeIfQTBLu1PWS%2F0abcdwpVoZp6dvHsBFfc%2FWDGeRJaHlTYL8liMJrt7g7lu%2BvDFSTN6Hvdoxgz6DEETBJEJH4mN23paM1xNm8YMzOT9brwWVuKemi6%2BP0S7D3NkRzck6a946%2BZ5hTGlZiRT9FczbFWrkSICE7%2FQ5wRIm9LmZgWnxTi4cLkURscp6j1w0tTkDTGgYXeIp9WH1aDNEBaqTWbn3X1aRqhyOqajiX8dka0oVddQ8enyMiZuHHM7RLfdIlpyYhdSHupy7IUphrmpe%2BzFhPixk6d4gOsk7shIa2Grt73HOOVaG2OwRYVnMj7btFZBPGE4Xc2wSoBAY5t%2FFYu0A5qkwksX4yAY6rQKU%2Fpry2s3kZpryMQp%2BXXEHurESdNzElYdyulKtWwdqR7uXZ03lkEcekEvsyvL1khvCM%2BzrwWmPfrJ7HduAqOliio9ZV0iahToDzfZgAuC3ld0W04xPj56w2grhbjVIgN2O8FO0g3lYCakrYdfuYidpNFA4w6PzSm%2BUYaIJNjdulMvsjaPtA3haALMJiS1us%2FyX0cv7nAx18DtBqyq8ScNp18%2FSupIKE%2BlCzcArhGfgfSSSijpLmPeDET1Np5VFx6RzXXvU2RfFyYRjOnN8UPKZAzyuXf8UEE1t67Lm7p6WeUA%2FA4%2FVNcWgqufvwoNMaZIe%2BKCqe%2BtruA6%2FwDdinMRo8pwXYC97teQ4NA62KP%2FqEbG3bQNL7%2FnJ3gpW1Ej29nUcKJfYHC0fpDNJoqr6&X-Amz-Signature=762110e83f00905806675ea9cb7971a8928f00a19f8f9d52f13b03e09e967073&X-Amz-SignedHeaders=host&response-content-disposition=inline'
+            href='https://ruth-anam-fullstack-resume.s3.us-east-2.amazonaws.com/Ruth+Anam_resume_25.pdf'
           >
             Fullstack Resume
           </a>
